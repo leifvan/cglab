c2b19b8a1b29efbbc77566441108240eecd5cf50
Leif Van Holland <holland@uni-bonn.de>
2020-06-20 17:03:37 +0200

    add some comments

--- displacement.py
+++ displacement.py
@@ -3,6 +3,19 @@ import scipy.interpolate
 
 
 def calculate_dense_displacements(assignments, distances, directions, smooth):
+    """
+    Calculates a displacement map based on binary assignments using L2-regularized radial basis
+    functions.
+
+    :param assignments: An array (n_angles, height, width) of binary maps s.t. assignments[k, i, j]
+        is 1 if pixel [i,j] corresponds to angle k (0 otherwise).
+    :param distances: An array (n_angles, height, width) of distances to the next edge pixel, i.e.
+        distances[k, i, j] is the distance of pixel [i,j] to the next edge pixel with angle k.
+    :param directions: An array (n_angles, height, width) of angles to the next edge pixel, i.e.
+        directions[k, i, j] is the angle from [i,j] to the next edge pixel with angle k.
+    :param smooth: L2-regularization factor for the RBFs.
+    :return:
+    """
     height, width = distances.shape[1:3]
     yy, xx = np.mgrid[:height, :width]
 
@@ -23,6 +36,7 @@ def calculate_dense_displacements(assignments, distances, directions, smooth):
 
     feature_gradient_cartesian = np.array([np.sin(feature_gradient_angles), np.cos(feature_gradient_angles)])
     feature_gradient_cartesian *= feature_gradient_magnitudes
+    # TODO check if we should change the interpolation mode='N-D' (because default is '1-D')
     interpolator_y = scipy.interpolate.Rbf(fy, fx, feature_gradient_cartesian[0, fy, fx],
                                            function='linear', smooth=smooth)
     interpolator_x = scipy.interpolate.Rbf(fy, fx, feature_gradient_cartesian[1, fy, fx],
--- distance_transform.py
+++ distance_transform.py
@@ -1,10 +1,20 @@
 import numpy as np
 from scipy.ndimage import distance_transform_edt
-
 from gradient_directions import get_gradients_in_polar_coords
 
 
 def get_binary_assignments_from_centroids(image, centroids, intervals):
+    """
+    Assigns each pixel of ``image`` with non-vanishing gradient to one of the angles in
+    ``centroids`` based on the given intervals.
+
+    :param image: The 2D input image of shape (height, width).
+    :param centroids: An array (n_angles,) of angles.
+    :param intervals: An array (n_angles, 2) of interval borders for each angle in ``centroids``.
+    :return: An array (n_angles, height, width) of binary maps for each centroid that assign a
+        pixel with non-vanishing gradient to exactly one of the centroids, i.e. if index [k,i,j] is
+        1, the pixel ``image[i,j]`` is assigned to angle ``centroids[k]``.
+    """
     angles, magnitudes = get_gradients_in_polar_coords(image)
     # get a binary map of features for every main angle
     pixel_assignments = np.zeros((len(centroids), *image.shape), dtype=np.bool)
@@ -21,6 +31,17 @@ def get_binary_assignments_from_centroids(image, centroids, intervals):
 
 
 def get_binary_assignments_from_gabor(responses, threshold):
+    """
+    Assigns each pixel with non-vanishing response to the filter with maximum absolute response.
+
+    :param responses: An array (n_filters, height, width) of filter responses from gabor filters.
+    :param threshold: All pixels with a response below this value will not be considered.
+    :return: An array (n_filters, height, width) of binary maps for each filter that assign a
+        pixel with non-vanishing response to exactly one of the filters, i.e. if index [k,i,j] is
+        1, the pixel [i,j] is assigned to filter k.
+    """
+
+    # TODO check if we can create gabor filters that aren't 180°-symmetric
     assignments = np.zeros_like(responses, dtype=np.bool)
     argmax = np.argmax(np.abs(responses), axis=0)
     for i, assignment in enumerate(assignments):
@@ -29,6 +50,13 @@ def get_binary_assignments_from_gabor(responses, threshold):
 
 
 def get_distance_transforms_from_binary_assignments(assignments):
+    """
+    Calculates the euclidean distance transform for each of the binary assignments given.
+
+    :param assignments: An array (n_angles, height, width) with binary assignments for each pixel.
+    :return: An array (n_angles, height, width) of distance transforms, where index [k,i,j] contains
+        the euclidean distance of pixel [i,j] to its nearest edge pixel.
+    """
     distance_transforms = np.zeros_like(assignments, dtype=np.float32)
 
     for distance_transform, assignment in zip(distance_transforms, assignments):
@@ -38,6 +66,14 @@ def get_distance_transforms_from_binary_assignments(assignments):
 
 
 def get_closest_feature_directions_from_binary_assignments(assignments):
+    """
+    Calculates the angle between each pixel to its closest pixel in assignments.
+
+    :param assignments: An array (n_angles, height, width) with binary assignments for each pixel.
+    :return: An array (n_angles, height, width) of angles, where index [k,i,j] contains the
+        angle of pixel [i,j] to its closest edge pixel.
+    """
+
     directions_list = np.zeros_like(assignments, dtype=np.float32)
     grid = np.mgrid[:assignments.shape[1], :assignments.shape[2]]
 
