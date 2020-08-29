127c0ca0e0466a304d596dc7f45b1733bc6e811f
Leif Van Holland <holland@uni-bonn.de>
2020-08-29 17:44:39 +0200

    add info return for bcpd wrapper

--- bcpd_wrapper.py
+++ bcpd_wrapper.py
@@ -1,18 +1,21 @@
 from pathlib import Path
 from subprocess import run
 import numpy as np
+import csv
 
 BCPD_DIR = Path("bcpd_win")
 
 _param_aliases = dict(omega='k', lamb='l', kappa='k', gamma='g')
 
 
-def run_bayesian_coherent_point_drift(target_set, source_set, output="y", kernel=None, **kwargs):
+def run_bayesian_coherent_point_drift(target_set, source_set, output="y", kernel=None,
+                                      return_info=False, **kwargs):
     np.savetxt(BCPD_DIR / "X.txt", target_set, delimiter=',')
     np.savetxt(BCPD_DIR / "Y.txt", source_set, delimiter=',')
 
     # kwargs to string
     kwargs = {_param_aliases.get(k, k): v for k, v in kwargs.items()}
+    assert all(len(k) == 1 for k in kwargs)
 
     params_list = [f"-s{output}"]
 
@@ -28,14 +31,22 @@ def run_bayesian_coherent_point_drift(target_set, source_set, output="y", kernel
     params = ' '.join(params_list)
     run(f"{BCPD_DIR / 'bcpd.exe'} {params}", cwd=BCPD_DIR)
 
+    output = output.replace("T", "sRt")
+
     return_values = []
     for out_char in output:
         return_values.append(np.loadtxt(BCPD_DIR / f"output_{out_char}.txt"))
 
     if len(return_values) == 1:
-        return return_values[0]
+        return_values = return_values[0]
 
-    return tuple(return_values)
+    if return_info:
+        with open(BCPD_DIR / f"output_info.txt") as file:
+            data = csv.reader(file, delimiter="\t")
+            info = {k: float(v) for k,v in data}
+        return return_values, info
+
+    return return_values
 
 
 if __name__ == '__main__':
